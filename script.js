39386552a4543c11e01771a5a17bf986f9c68048
Annabel-13 <AnnabelFeer@gmail.com>
2024-05-28 21:46:19 +0200

    third block, screenshots, footer

--- script.js
+++ script.js
@@ -7,3 +7,12 @@ document.addEventListener('DOMContentLoaded', function() {
         nextBlock.scrollIntoView({ behavior: 'smooth' });
     });
 });
+
+
+window.addEventListener('scroll', function() {
+    let button = document.querySelector('.arrow');
+    let scrollPosition = window.scrollY;
+
+    // Update button position based on scroll position
+    button.style.top = (scrollPosition + window.innerHeight - button.offsetHeight - 20) + 'px';
+});
